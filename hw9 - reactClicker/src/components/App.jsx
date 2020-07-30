ef5cbe205b75dbf28dd38f30d6849633841d79cf
kaziuchytsValerie <kaziuchytsvalerie@gmail.com>
2020-07-30 18:42:04 +0300

    Add homework #9 (App.jsx)

---
+++ hw9 - reactClicker/src/components/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Clicker } from './Clicker.jsx';
+
+function App() {
+  return (
+    <div className="appContainer">
+      <Clicker />
+      <Clicker />
+      <Clicker />
+      <Clicker />
+      <Clicker />
+    </div>
+  );
+}
+
+export default App;
