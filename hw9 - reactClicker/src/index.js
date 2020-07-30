7763e8e838790bb55d312d18b27742eb995f38ea
kaziuchytsValerie <kaziuchytsvalerie@gmail.com>
2020-07-30 18:39:31 +0300

    Add homework #9 (index.js)

---
+++ hw9 - reactClicker/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App.jsx';
+import './assets/CSS/index.css';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root'),
+);
